15f9c6cb43444c034aa8bcde9081a73b68e72dde
Ira <ira.gitunets23@gmail.com>
2024-12-10 00:43:39 +0200

    add details.html and detailsStyle.css

--- js/script.js
+++ js/script.js
@@ -1,50 +1,57 @@
 async function getProducts() {
-    // Виконуємо запит до файлу "store_db.json" та очікуємо на відповідь
-    let response = await fetch("store_db.json");
-    // Очікуємо на отримання та розпакування JSON-даних з відповіді
-    let products = await response.json();
-  
-    // Повертаємо отримані продукти
-    return products;
-  }
+  // Виконуємо запит до файлу "store_db.json" та очікуємо на відповідь
+  let response = await fetch("store_db.json");
+  // Очікуємо на отримання та розпакування JSON-даних з відповіді
+  let products = await response.json();
 
-  // Генеруємо HTML-код для карточки товару
+  // Повертаємо отримані продукти
+  return products;
+}
+
+// Генеруємо HTML-код для карточки товару
 function getCardHTML(product) {
-    // Створюємо JSON-строку з даними про товар і зберігаємо її в data-атрибуті
-    let productData = JSON.stringify(product);
-    return `
-          <div class="card" data-id=${product.id} style="width: 18rem;">
-          <img src="img/${product.image}">
-              <h5 class="card-title">${product.title}</h5>
-              <p class="description-card">${product.description}</p>
-              <p class="card-text"> ${product.price}</p>
-              <a href="#" class="btn btn-primary cart-btn"  data-product='${productData}'>В кошик</a>
-          </div>
-          </div>
-           `;
+  // Створюємо JSON-строку з даними про товар і зберігаємо її в data-атрибуті
+  let productData = JSON.stringify(product);
+  return `
+    <div class="card" id="product-${product.id}" data-id="${product.id}" style="width: 18rem;">
+      <img src="img/${product.image}" alt="${product.title}">
+      <p class="card-weight">${product.weight}</p>
+      <h5 class="card-title">${product.title}</h5>
+      <p class="description-card">${product.description}</p>
+      <p class="card-text">${product.price}</p>
+      <a href="#" class="btn btn-primary cart-btn" data-product='${productData}'>В кошик</a>
+    </div>
+  `;
+}
+
+// This function will be called after products are fetched
+function renderProducts(products) {
+  const productsList = document.querySelector(".products-list");
+  if (productsList) {
+    products.forEach((product) => {
+      // Відображаємо товари на сторінці
+      productsList.innerHTML += getCardHTML(product);
+    });
+  } 
+}
+
+    // Делегування подій
+function attachProductClickListener() {
+  const productsList = document.querySelector(".products-list");
+  if (productsList) {
+    productsList.addEventListener("click", function (event) {
+      const card = event.target.closest(".card");
+      if (card) {
+        const cardId = card.getAttribute("data-id");
+        console.log(`Card clicked with unique ID: ${cardId}`);
+        window.location.href = `details.html?id=${cardId}`;
+      }
+    });
   }
+}
 
-
-
-  getProducts().then(function (products) {
-    let productsList = document.querySelector(".products-list");
-    if (productsList) {
-      products.forEach(function (product) {
-        // Відображаємо товари на сторінці
-        productsList.innerHTML += getCardHTML(product);
-      });
-    }
-  });
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  const cards = document.querySelectorAll(".card");
-
-  cards.forEach(function (card) {
-    card.addEventListener("click", function () {
-      const cardId = card.getAttribute("product.id") ;
-      window.location.href = `details.html?id=${cardId}`;
-    })
-  });
+// Завантажуємо продукти та підключаємо слухача
+getProducts().then((products) => {
+  renderProducts(products);
+  attachProductClickListener();
 });
-
